595126c1c116a0c94e2435d11501330cb743fb54
Kirill <xfiendx4life@gmail.com>
2021-11-18 13:23:51 +0300

    Initialized logs

--- hw2/main.go
+++ hw2/main.go
@@ -6,20 +6,47 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/xfiendx4life/gb_gobest/hw2/files_deleter"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
-	delete  bool
-	dir     string
-	ErrHelp = errors.New("flag: help requested")
+	delete   bool
+	dir      string
+	ErrHelp  = errors.New("flag: help requested")
+	logLevel = zap.LevelFlag("loglevel", zap.InfoLevel, "set logging level")
+	filelog  string
+	logger   *zap.SugaredLogger
 )
 
 func init() {
 	flag.BoolVar(&delete, "delete", false, "set true if you want to delete duplicate files")
 	flag.StringVar(&dir, "dir", ".", "choose direcory to work with")
+	flag.StringVar(&filelog, "filelog", "", "choose file for logs, leave empty to use stderr")
+
+}
+
+func InitLogger(level *zapcore.Level, filelog string) {
+	var output io.Writer
+	var encoder zapcore.Encoder
+	// choosing file or stderr
+	if filelog != "" {
+		output, _ = os.Create(filelog)                                     // we are going to use file as log output
+		encoder = zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()) // using json for file
+	} else {
+		output = os.Stderr
+		encoder = zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()) // using simple console
+
+	}
+
+	writeSyncer := zapcore.AddSync(output)
+	core := zapcore.NewCore(encoder, writeSyncer, level)
+	logger = zap.New(core).Sugar()
+
 }
 
 func main() {
@@ -29,6 +56,11 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+	InitLogger(logLevel, filelog)
+	logger.Debugf("Checking debug")
+	logger.Errorf("Checking error")
+	logger.Info("checking info")
 	n, _ := files_deleter.Delete(dir, delete)
 	fmt.Println(n)
+
 }
